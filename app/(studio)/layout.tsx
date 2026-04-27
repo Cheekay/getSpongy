eeb85519e1af4adf0982f5a68253ef16111b2d66
Chike O. <chikeonyema@gmail.com>
2026-04-27 19:57:19 -0400

    fix: add id=main-content to manage and studio layouts for skip link

--- app/(studio)/layout.tsx
+++ app/(studio)/layout.tsx
@@ -3,7 +3,7 @@ import { BottomNav } from '@/components/ui/BottomNav'
 export default function StudioLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col min-h-screen">
-      <div className="flex-1 pb-20">
+      <div id="main-content" className="flex-1 pb-20">
         {children}
       </div>
       <BottomNav variant="studio" />
